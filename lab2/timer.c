d19fc550981ff8a27f76e532467566b3d3923f77
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-03-28 16:34:01 +0000

    Finished timer set freq

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,60 @@
 #include "i8254.h"
 
 int(timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  // Validate input.
+  if (timer < 0 || timer > 2)
+    return 1;
 
-  return 1;
+  if (freq < 19 || freq > 1193181)
+    return 1;
+
+  // Read current config.
+  uint8_t currentConfig;
+  if (timer_get_conf(timer, &currentConfig))
+    return 1;
+
+  // Calculate the initial value for the timer.
+  uint16_t initialValue = 1193181 / freq;
+
+  // Split the initial value into LSB and MSB.
+  uint8_t initialValueMSB, initialValueLSB;
+  if (util_get_MSB(initialValue, &initialValueMSB))
+    return 1;
+  if (util_get_LSB(initialValue, &initialValueLSB))
+    return 1;
+
+  // Create the control word.
+  uint8_t controlWord = 0;
+
+  // Start by choosing the timer (bits 7 and 6).
+  switch (timer) {
+    case 1:
+      controlWord |= 0x40; // 01xxxxxx
+      break;
+    case 2:
+      controlWord |= 0x80; // 10xxxxxx
+      break;
+    default:
+      break;
+  }
+
+  // Set the initialization mode to LSB followed by MSB (bits 5 and 4).
+  controlWord |= 0x30; // xx11xxxx
+
+  // The remaining bits should be that same as the current config.
+  controlWord |= currentConfig & 0x0F;
+
+  // Write the control word to the control register (0x43).
+  if (sys_outb(0x43, controlWord))
+    return 1;
+
+  // Write the LSB and the MSB to the timer.
+  if (sys_outb(0x40 + timer, initialValueLSB))
+    return 1;
+  if (sys_outb(0x40 + timer, initialValueMSB))
+    return 1;
+
+  return 0;
 }
 
 int(timer_subscribe_int)(uint8_t *bit_no) {
