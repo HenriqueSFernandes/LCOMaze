fd275c398473571969ccca3402e7cfe6fd43028c
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-03-30 16:10:47 +0000

    Finished lab3 scan

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,15 +1,13 @@
-#include "keyboard.h"
-#include "timer.c"
-#include <i8042.h>
-#include <lcom/lab3.h>
 #include <lcom/lcf.h>
+
+#include <lcom/lab3.h>
+
 #include <stdbool.h>
 #include <stdint.h>
 
-extern uint8_t valueKBD;
-extern uint32_t counter;
-extern int counterTimer;
-extern uint8_t r;
+int kbd_hook_id = 1;
+uint8_t kbd_value = 0x00;
+extern uint32_t sysinb_calls;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -35,102 +33,63 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+int(kbd_subscribe_int)(uint8_t *irq_set) {
+  // Validate input.
+  if (irq_set == NULL)
+    return 1;
+
+  // The irq_line should be the position of the hook id.
+  *irq_set = BIT(kbd_hook_id);
+
+  // In addition to IRQ_REENABLE, the policy also needs to be exclusive, because MINIX has a builtin interrupt handler for the keyboard.
+  return sys_irqsetpolicy(1, IRQ_REENABLE | IRQ_EXCLUSIVE, &kbd_hook_id);
+}
+
+int(kbd_unsubscribe_int)() {
+  return sys_irqrmpolicy(&kbd_hook_id);
+}
+
+int(kbd_ih)() {
+  // Read the status from the KBC.
+  uint8_t status;
+  if (util_sys_inb(0x64, &status))
+    return 1;
+
+  // Check for error in the status.
+  if (status & BIT(7)) {
+    printf("Parity error!\n");
+    return 1;
+  }
+  if (status & BIT(6)) {
+    printf("Timeout error!\n");
+    return 1;
+  }
+  if (status & BIT(1)) {
+    printf("The input buffer is full!\n");
+    return 1;
+  }
+
+  // If the output buffer is full, data is available for reading.
+  if ((status & BIT(0)) != 0) {
+    return util_sys_inb(0x60, &kbd_value);
+  }
+  return 1;
+}
+
 int(kbd_test_scan)() {
-  uint8_t res[2];
-  int ipc_status;
-  bool waiting = false;
-  message msg;
-  uint8_t irq_set;
-  if (kbd_subscribe_int(&irq_set))
-    return 1;
-  int error;
-  while (valueKBD != 0x81) {
-    if ((error = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-      printf("error driver_receive");
-      return 1;
-    }
-    if (is_ipc_notify(ipc_status)) {
-      switch (_ENDPOINT_P(msg.m_source)) {
-        case HARDWARE:
-          if (msg.m_notify.interrupts & irq_set) {
-            kbc_ih();
-            if (waiting) {
-              res[1] = valueKBD;
-            }
-            else {
-              res[0] = valueKBD;
-            }
-            if (valueKBD == 0xE0) {
-              waiting = true;
-              break;
-            }
-            if (valueKBD != 0xE0) {
-              uint8_t x = waiting ? 2 : 1;
-              kbd_print_scancodde(!(valueKBD & MAKE_CODE), x, res);
-            }
-            waiting = false;
-          }
-          break;
-        default:
-          break;
-      }
-    }
-  }
-  kbd_print_no_sysinb(counter);
-  return kbd_unsubscribe_int();
-}
+  int ipc_status;  // Indicates if a message has been received.
+  int receiver;    // Indicates if there was an error receiving the message.
+  uint8_t irq_set; // 8-bit value that indicates which interrupts the program should care about.
+  message msg;     // The message itself.
+  uint8_t keys[2] = {0x00, 0x00};
 
-int(kbd_test_poll)() {
-  uint8_t res[2];
-  bool waiting = false;
-  while (valueKBD != 0x81) {
-    uint8_t status;
-    if (util_sys_inb(STATUS_REG, &status)) {
-      return 1;
-    };
-    if ((status & (BIT(6) | BIT(7))) != 0) {
-      return 1;
-    }
-    if (status & BIT(0)) {
-      if (util_sys_inb(0x60, &valueKBD)) {
-        return 1;
-      };
-      if (waiting) {
-        res[1] = valueKBD;
-      }
-      else {
-        res[0] = valueKBD;
-      }
-      if (valueKBD == 0xE0) {
-        waiting = true;
-      }
-      if (valueKBD != 0xE0 && valueKBD) {
-        uint8_t x = waiting ? 2 : 1;
-        kbd_print_scancode(!(valueKBD & MAKE_CODE), x, res);
-      }
-      waiting = false;
-    }
-  }
-
-  kbd_print_no_sysinb(counter);
-  return restoreKeyboard();
-}
-
-int(kbd_test_timed_scan)(uint8_t n) {
-  uint8_t res[2];
-  bool waiting = false;
-  int receiver;
-  int ipc_status;
-  uint8_t irq_set_timer, irq_set_kbd;
-  message msg;
-  uint8_t time = 0;
-
-  if (timer_subscribe_int(&irq_set_timer) != 0)
-    return 1;
-  if (kbd_subscribe_int(&irq_set_kbd) != 0)
+  // Subscribe to the interruptions.
+  if (kbd_subscribe_int(&irq_set) != 0)
     return 1;
 
-  while (valueKBD != 0x81 && time < n) {
+  // Loop until the defined time has passed.
+  while (kbd_value != 0x81) {
+    // Check if there is a message available
     if ((receiver = driver_receive(ANY, &msg, &ipc_status)) != 0) {
       printf("error driver_receive");
       return 1;
@@ -138,29 +97,22 @@ int(kbd_test_timed_scan)(uint8_t n) {
     if (is_ipc_notify(ipc_status)) {
       switch (_ENDPOINT_P(msg.m_source)) {
         case HARDWARE:
-          if (msg.m_notify.interrupts & irq_set_timer) {
-            timer_int_handler();
-            if (counterTimer % 60 == 0)
-              time++;
-          }
-          if (msg.m_notify.interrupts & irq_set_kbd) {
-            time = 0;
-            kbc_ih();
-            if (waiting) {
-              res[1] = valueKBD;
+          // If the interrupt is for the keyboard, call the interrupt handler.
+          if (msg.m_notify.interrupts & irq_set) {
+            // Call the interrupt handler.
+            if (kbd_ih())
+              return 1;
+            // If the previous key was 0xE0, then it means the makecode and breakcode for that key has size 2.
+            if (keys[0] == 0xE0) {
+              keys[1] = kbd_value;
             }
             else {
-              res[0] = valueKBD;
+              keys[0] = kbd_value;
             }
-            if (valueKBD == 0xE0) {
-              waiting = true;
-              break;
+            if (kbd_value != 0xE0) {
+              kbd_print_scancode((kbd_value & BIT(7)) == 0, keys[0] == 0xE0 ? 2 : 1, keys);
+              keys[0] = 0x00;
             }
-            if (valueKBD != 0xE0) {
-              uint8_t x = waiting ? 2 : 1;
-              kbd_print_scancode(!(valueKBD & MAKE_CODE), x, res);
-            }
-            waiting = false;
           }
           break;
         default:
@@ -168,9 +120,20 @@ int(kbd_test_timed_scan)(uint8_t n) {
       }
     }
   }
-  if (kbd_unsubscribe_int())
-    return 1;
-  if (timer_unsubscribe_int())
-    return 1;
-  return 0;
+  kbd_print_no_sysinb(sysinb_calls);
+  return kbd_unsubscribe_int();
+}
+
+int(kbd_test_poll)() {
+  /* To be completed by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int(kbd_test_timed_scan)(uint8_t n) {
+  /* To be completed by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
 }
